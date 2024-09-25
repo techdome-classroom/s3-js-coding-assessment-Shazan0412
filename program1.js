7fd84d999187deb93f7f97970d231eee8a175da5
Md Shazan Asjad <asjadshazan5@gmail.com>
2024-09-25 15:38:02 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,7 +3,27 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+  function isValid(s) {
+    const stack = [];
+    const mapping = {
+      ')': '(',
+      '}': '{',
+      ']': '['
+    };
+  
+    for (let i = 0; i < s.length; i++) {
+      const char = s[i];
+      if (char === '(' || char === '{' || char === '[') {
+        stack.push(char);
+      } else {
+        if (stack.length === 0 || mapping[char] !== stack.pop()) {
+          return false;
+        }
+      }
+    }
+  
+    return stack.length === 0;
+  }
 };
 
 module.exports = { isValid };
