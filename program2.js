e282fb39a49e3623082bdc2e2111092c8325b00d
Md Shazan Asjad <asjadshazan5@gmail.com>
2024-09-25 15:44:16 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,7 +3,30 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    function romanToInt(s) {
+        const romanMap = {
+          I: 1,
+          V: 5,
+          X: 10,
+          L: 50,
+          C: 100,
+          D: 500,
+          M: 1000
+        };
+      
+        let result = 0;
+        for (let i = 0; i < s.length; i++) {
+          const currentChar = s[i];
+          const nextChar = s[i + 1];
+          if (romanMap[currentChar] < romanMap[nextChar]) {
+            result -= romanMap[currentChar];
+          } else {
+            result += romanMap[currentChar];
+          }
+        }
+      
+        return result;
+      }
 };
 
 
