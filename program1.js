be00d3beba0ee34092c37e26c9b2534e302eb3db
Md Shazan Asjad <asjadshazan5@gmail.com>
2024-09-25 15:30:20 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,65 +3,7 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    // Stack module
-class Stack {
-    constructor() {
-      this.items = [];
-    }
-  
-    push(item) {
-      this.items.push(item);
-    }
-  
-    pop() {
-      return this.items.pop();
-    }
-  
-    isEmpty() {
-      return this.items.length === 0;
-    }
-  }
-  
-  // Mapping module
-  const mapping = {
-    ')': '(',
-    '}': '{',
-    ']': '['
-  };
-  
-  // Validator module
-  class Validator {
-    isValid(s) {
-      const stack = new Stack();
-  
-      for (let i = 0; i < s.length; i++) {
-        const char = s[i];
-        if (this.isOpenBracket(char)) {
-          stack.push(char);
-        } else {
-          if (this.isInvalidClosingBracket(stack, char)) {
-            return false;
-          }
-        }
-      }
-  
-      return stack.isEmpty();
-    }
-  
-    isOpenBracket(char) {
-      return ['(', '{', '['].includes(char);
-    }
-  
-    isInvalidClosingBracket(stack, char) {
-      return stack.isEmpty() || mapping[char] !== stack.pop();
-    }
-  }
-  
-  // Usage
-  const validator = new Validator();
-  console.log(validator.isValid("()")); // true
-  console.log(validator.isValid("()[]{}")); // true
-  console.log(validator.isValid("(]")); // false
+    
 };
 
 module.exports = { isValid };
